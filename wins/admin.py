98b69c3ac9eeba91ae4c1efa7405e5933932cabf
chriskaringeg <ckaringe@gmail.com>
2019-05-27 09:45:00 +0300

    add content in app

--- wins/admin.py
+++ wins/admin.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import User, Image , Profile
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
